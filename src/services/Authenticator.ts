ca7ef1ca0814a3895f2f83ad09015a578de8a5b8
micheline-barros <michelinemms@gmail.com>
2021-11-15 14:59:21 -0300

    model user

--- src/services/Authenticator.ts
+++ src/services/Authenticator.ts
@@ -1,32 +1,30 @@
-import * as jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken"
+import dotenv from 'dotenv';
+import { AuthenticationData } from "../model/User";
+
+
+dotenv.config()
 
 export class Authenticator {
-  public generateToken(input: AuthenticationData,
-    expiresIn: string = process.env.ACCESS_TOKEN_EXPIRES_IN!): string {
-    const token = jwt.sign(
-      {
-        id: input.id,
-        role: input.role
-      },
-      process.env.JWT_KEY as string,
-      {
-        expiresIn,
-      }
-    );
-    return token;
-  }
+   generateToken = (
+      payload: AuthenticationData
+   ): string => {
+      return jwt.sign(
+         payload,
+         process.env.JWT_KEY as string,
+         {
+            expiresIn: process.env.EXPIRES_IN
+         }
+      )
+   }
 
-  public getData(token: string): AuthenticationData {
-    const payload = jwt.verify(token, process.env.JWT_KEY as string) as any;
-    const result = {
-      id: payload.id,
-      role: payload.role
-    };
-    return result;
-  }
-}
+   getTokenData = (
+      token: string
+   ): AuthenticationData => {
+      return jwt.verify(
+         token,
+         process.env.JWT_KEY as string
+      ) as AuthenticationData
+   }
 
-interface AuthenticationData {
-  id: string;
-  role?: string;
 }

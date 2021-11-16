636d084b9304675597ff67ef3f2fd8c06accff96
micheline-barros <michelinemms@gmail.com>
2021-11-16 16:29:31 -0300

    correção cpf tabela

--- src/data/CreateTables.ts
+++ src/data/CreateTables.ts
@@ -15,7 +15,7 @@ export class CreateTables extends BaseDatabase{
             CREATE TABLE IF NOT EXISTS cliente_table(
                 id VARCHAR(255) PRIMARY KEY,
                 nome VARCHAR(255) NOT NULL,
-                cpf VARCHAR(255) NOT NULL,
+                cpf VARCHAR(255) UNIQUE NOT NULL,
                 telefone VARCHAR(255) UNIQUE NOT NULL,
                 email VARCHAR(255) UNIQUE NOT NULL,
                 logradouro VARCHAR(255) NOT NULL,
